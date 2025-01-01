839083adbd39a0be3a6a0e4cda0e6f1f113d9268
Jsfgefge <68561882+Jsfgefge@users.noreply.github.com>
2025-01-01 12:31:29 -0600

    git pluggin added

---
+++ lua/config/plugins/git.lua
@@ -0,0 +1,14 @@
+return {
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require('gitsigns').setup {
+				signs = {
+					delete = { text = "󰍵" },
+					changedelete = { text = "󱕖" },
+				},
+			}
+		end
+	}
+
+}
